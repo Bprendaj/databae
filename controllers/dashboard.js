80a8d192956dccb2848bd5ee0edd6233af2e4b21
Kate Bonner <katebonner277@gmail.com>
2022-07-28 18:37:50 -0400

    send nodes

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -5,9 +5,23 @@ const withAuth = require('../utils/auth');
 
 // RENDER DASHBOARD
 router.get('/', withAuth, async (req, res) => {
-    res.render('dashboard', {
-        loggedIn: req.session.loggedIn
-    });
+    try {
+        const userData = await User.findAll({
+            attributes: { exclude: ['password'] }
+          });
+
+        const users = userData.map((user) =>
+            user.get({ plain: true })
+        );
+
+        res.render('dashboard', {
+            users,
+            loggedIn: req.session.loggedIn
+        });
+    } catch {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
