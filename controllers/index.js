dcf987d80cefbe671713c153c89bc998b71e074b
Kate Bonner <katebonner277@gmail.com>
2022-07-28 13:20:44 -0400

    chat button

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home')
 const dashboardRoutes = require('./dashboard')
+const chatRoutes = require("./chat");
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
+router.use('/chat', chatRoutes);
 
 module.exports = router;
